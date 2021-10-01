067bf794a9ab85cdfb4437a6ee0b2081ab2cf142
dpbam <derekphtpt@gmail.com>
2021-10-01 15:30:32 -0600

    for askbcs, server not running

--- server/server.js
+++ server/server.js
@@ -3,10 +3,40 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas/');
+
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import the middleware function
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const startServer = async () => {
+  // create a new Apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+  // Start the Apollo server
+  await server.start();
+
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// initialize the Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
